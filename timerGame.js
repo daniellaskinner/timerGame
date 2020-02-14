1608883f76e982fb7de146536b2b3687d5feaec7
daniellaskinner <daniellaskinner120@gmail.com>
2020-02-14 15:49:02 +0000

    added game over messages

--- timerGame.js
+++ timerGame.js
@@ -15,32 +15,27 @@ playButton.addEventListener("click", clicker)
 let seconds = 3
 let timerInterval = 0
 
+//to stop the timer, declare a variable and put the set interval function in this variable then clear interval on this one
 function timerCountdown() {
     timerInterval = setInterval(function() {
         seconds--
         document.getElementById("timerBox").textContent = "Timer: " + seconds
         if(!seconds) {
-            playButton.removeEventListener("click", clicker)
-            clearInterval(timerInterval)
-            alert("Time's up!" + " You got a score of: " + playButtonClickCounter)
-
+            if (playButtonClickCounter<10 && playButtonClickCounter>=1) {
+                playButton.removeEventListener("click", clicker)
+                clearInterval(timerInterval)
+                alert("Time's up! Well you're crap at this! You only scored " + playButtonClickCounter + " points!")
+            } else if (playButtonClickCounter < 1) {
+                playButton.removeEventListener("click", clicker)
+                clearInterval(timerInterval)
+                alert("Time's up! Oh dear what happened!?! You scored zero!")
+            } else {
+                playButton.removeEventListener("click", clicker)
+                clearInterval(timerInterval)
+                alert("Time's up! You're great! You got a score of: " + playButtonClickCounter + " points!")
+            }
         }
     }, 1000)
 }
 
 timerCountdown()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//to stop the timer, declare a variable and put the set interval function in a variable then clear interval on this one
